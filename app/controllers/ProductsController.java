2f7cef68ef529eb33151d2d2886484b3436eac93
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2016-03-27 17:37:20 -0700

    Action methods and routes added.

---
+++ app/controllers/ProductsController.java
@@ -0,0 +1,23 @@
+package controllers; 
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ProductsController extends Controller {
+
+  public Result list() {
+    return TODO;
+  }
+
+  public Result newProduct() {
+    return TODO;
+  }
+
+  public Result details(String ean) {
+    return TODO;
+  }
+
+  public Result save() {
+    return TODO;
+  }
+
+}
