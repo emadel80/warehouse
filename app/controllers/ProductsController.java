53c4a7d753083bc4ad07ec593577c848a3608bbd
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2016-04-02 12:15:24 -0700

    Implements products listing page.

--- app/controllers/ProductsController.java
+++ app/controllers/ProductsController.java
@@ -2,23 +2,27 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import java.util.List;
+import models.Product;
+import views.html.products.list;
 
 public class ProductsController extends Controller {
 
   public Result list() {
-    return TODO;
+    List<Product> products = Product.findAll();
+    return ok(list.render(products));
   }
 
   public Result newProduct() {
-    return TODO;
+    return ok();
   }
 
   public Result details(String ean) {
-    return TODO;
+    return ok();
   }
 
   public Result save() {
-    return TODO;
+    return ok();
   }
 
 }
